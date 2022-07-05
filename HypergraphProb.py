5352bb26498794e47bb803f5350f813bb5045760
andres marulanda <maruland@nuc190.wsnet>
2022-07-05 11:20:49 +0200

    Most optimized version of code, both memory and processing.

--- HypergraphProb.py
+++ HypergraphProb.py
@@ -1,61 +1,35 @@
+#!/usr/bin/python3
+
+from scipy.sparse import rand
+import matplotlib.pyplot as plt
+import numpy as np
+import click
+
+@click.command()
+@click.option("--nelems", type=int, default=10000, help="Number of elements in set V.")
+@click.option("--tsteps", type=int, default=10000, help="Number of time steps.")
+def main(nelems, tsteps):
+
+    # Chunk size to split the whole process duration into smaller processes
+    # This is for memory reasons. In the limit chunk_sz -> 1 we are memory efficient but slow.
+    # In 8GB, n*chunk_sz = 1e8 works in the limit
+    chunk_sz = 100             
+
+    deg_cum = np.zeros((1,nelems))   # Array to accumulate degree counts
+    n_chunks = tsteps//chunk_sz + 1
+    for i in range(n_chunks):    # Divide t steps, into chunks of chunk_sz steps.
+        print(f"{i}/{n_chunks}")
+        process = rand(chunk_sz , nelems , 0.5, format='csc') > 0.5
+        deg_cum += process.sum(axis=0)
+
+    dist = np.unique(np.array(deg_cum), return_counts=True)[1]
+
+    plt.plot(dist)
+    plt.show()
 
 
-# Ejemplo
-
-N = 1000
-V = np.arange(N)
-
-T = 100
-
-HG = []
-for t in range(T):
-
-    sample = []
-    for i in V:
-        elem = np.random.choice([0,1], p=[0.5, 0.5])
-        sample.append(elem)
-
-    HG.append(sample)
-
-c=np.zeros(N)
-for ha in HG:
-    for i,e in enumerate(ha):
-        if e==1:
-            c[i]+=1
+if __init__ == "__main__":
+    main()
 
 
-u, cnts = np.unique(c , return_counts=True)
-
-prob = cnts.astype(np.float64)/np.sum(cnts)
-
-
-
-########
-# Codigo optimizado 1.
-
-# Ejemplo
-
-N = 5000
-V = np.arange(N)
-
-T = 100
-
-HG = []
-for t in range(T):
-
-    sample = np.random.choice([0,1],
-                                 size=N,
-                                 p=[0.5, 0.5])
-    HG.append(sample)
-
-c=np.zeros(N)
-for ha in HG:
-    for i,e in enumerate(ha):
-        if e==1:
-            c[i]+=1
-
-
-u, cnts = np.unique(c , return_counts=True)
-
-prob = cnts.astype(np.float64)/np.sum(cnts)
 
